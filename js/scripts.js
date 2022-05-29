0fe5a443ed1293a431d76a27f5eb1d759e7865a4
Adrian Estopace <estopaceadrian@gmail.com>
2022-05-29 14:04:52 +0800

    updated links and updated index

--- js/scripts.js
+++ js/scripts.js
@@ -29,4 +29,21 @@ window.addEventListener("DOMContentLoaded", () => {
     }
     scrollPos = currentTop;
   });
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 3,
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
 });
